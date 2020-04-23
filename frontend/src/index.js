c381ec27ffde1e6a2fb1880ed18ca88e7bb0077d
archi-007 <archiribhu@gmail.com>
2020-04-23 17:45:07 +0530

    modified the pkg files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,14 @@
-console.log('here');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>hello</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
